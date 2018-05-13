2842816884776018d7e24eb23e1bd450b3724a91
bibek <bibek@bibek>
2018-05-13 09:35:42 +0545

    int32 ready

--- bobc/src/main/java/bobc/core/processing/fieldConverters/Int32FieldConversionProcessor.java
+++ bobc/src/main/java/bobc/core/processing/fieldConverters/Int32FieldConversionProcessor.java
@@ -1,31 +1,131 @@
 package bobc.core.processing.fieldConverters;
 
 import java.lang.annotation.Annotation;
+import java.math.BigInteger;
 import java.nio.ByteBuffer;
 
 import bobc.core.ByteOrder;
+import bobc.core.exception.BobcErrorCodes;
+import bobc.core.exception.BobcException;
 import bobc.core.processing.ConversionProcessor;
+import bobc.types.Int32Field;
 
 public class Int32FieldConversionProcessor implements ConversionProcessor {
 
 	@Override
 	public Integer getSize() {
-		// TODO Auto-generated method stub
-		return null;
+		return 32;
 	}
 
 	@Override
 	public Object fromBytes(Class<?> target, Annotation[] fieldAnnotations, ByteBuffer buffer,
 			Boolean allowLossyConversion, Boolean isSilent) {
-		// TODO Auto-generated method stub
+		int value = buffer.getInt();
+		if (target.equals(Byte.class) || target.equals(Byte.TYPE)) {
+			if (allowLossyConversion)
+				return (byte) value;
+			else {
+				if (!isSilent)
+					throw new BobcException(BobcErrorCodes.LOSSY_CONVERSION,
+							BobcErrorCodes.lossyConversionMsg(Int32Field.class, target));
+			}
+		} else if (target.equals(Short.class) || target.equals(Short.TYPE)) {
+			if (allowLossyConversion)
+				return (short) value;
+			else {
+				if (!isSilent)
+					throw new BobcException(BobcErrorCodes.LOSSY_CONVERSION,
+							BobcErrorCodes.lossyConversionMsg(Int32Field.class, target));
+			}
+		} else if (target.equals(Character.class) || target.equals(Character.TYPE)) {
+			if (allowLossyConversion)
+				return Character.forDigit(value, 10);
+			else {
+				if (!isSilent)
+					throw new BobcException(BobcErrorCodes.LOSSY_CONVERSION,
+							BobcErrorCodes.lossyConversionMsg(Int32Field.class, target));
+			}
+		} else if (target.equals(Integer.class) || target.equals(Integer.TYPE)) {
+			// int32 is int in java so no prob
+			return value;
+		} else if (target.equals(Long.class) || target.equals(Long.TYPE)) {
+			return (long) value;
+		} else if (target.equals(Float.class) || target.equals(Float.TYPE)) {
+			return (float) value;
+		} else if (target.equals(Double.class) || target.equals(Double.TYPE)) {
+			return (double) value;
+		} else if (target.equals(BigInteger.class)) {
+			return BigInteger.valueOf(value);
+		} else if (target.equals(String.class)) {
+			return String.valueOf(value);
+		} else {
+			if (!isSilent)
+				throw new BobcException(BobcErrorCodes.UNKNOWN_CONVERSION,
+						BobcErrorCodes.conversionNotKnown(Int32Field.class, target));
+		}
 		return null;
 	}
 
 	@Override
 	public byte[] fromField(Class<?> fieldType, Object fieldValue, Annotation[] fieldAnnotations, ByteOrder order,
 			Boolean allowLossyConversion, Boolean isSilent) {
-		// TODO Auto-generated method stub
-		return null;
+		ByteBuffer buffer = ByteBuffer.allocate(4).order(
+				order == ByteOrder.LITTLE_ENDIAN ? java.nio.ByteOrder.LITTLE_ENDIAN : java.nio.ByteOrder.BIG_ENDIAN);
+		if (fieldValue == null) {
+			throw new BobcException(BobcErrorCodes.PACKING_WITH_NULL, BobcErrorCodes.packingWithNull(fieldType));
+		}
+
+		if (fieldType.equals(Byte.class) || fieldType.equals(Byte.TYPE)) {
+			buffer.putInt((byte) fieldValue);
+		} else if (fieldType.equals(Short.class) || fieldType.equals(Short.TYPE)) {
+			buffer.putInt((short) fieldValue);
+		} else if (fieldType.equals(Character.class) || fieldType.equals(Character.TYPE)) {
+			buffer.putInt(Integer.valueOf(String.valueOf(fieldValue)));
+		} else if (fieldType.equals(Integer.class) || fieldType.equals(Integer.TYPE)) {
+			buffer.putInt((int) fieldValue);
+		} else if (fieldType.equals(Long.class) || fieldType.equals(Long.TYPE)) {
+			// long to int32 is loss conv
+			if (allowLossyConversion)
+				buffer.putInt((int) (long) fieldValue);
+			else {
+				if (!isSilent)
+					throw new BobcException(BobcErrorCodes.LOSSY_CONVERSION,
+							BobcErrorCodes.lossyConversionMsg(fieldType, Int32Field.class));
+			}
+		} else if (fieldType.equals(Float.class) || fieldType.equals(Float.TYPE)) {
+			if (allowLossyConversion)
+				buffer.putInt((int) (float) fieldValue);
+			else {
+				if (!isSilent)
+					throw new BobcException(BobcErrorCodes.LOSSY_CONVERSION,
+							BobcErrorCodes.lossyConversionMsg(fieldType, Int32Field.class));
+			}
+		} else if (fieldType.equals(Double.class) || fieldType.equals(Double.TYPE)) {
+			if (allowLossyConversion)
+				buffer.putInt((int) (double) fieldValue);
+			else {
+				if (!isSilent)
+					throw new BobcException(BobcErrorCodes.LOSSY_CONVERSION,
+							BobcErrorCodes.lossyConversionMsg(fieldType, Int32Field.class));
+			}
+		} else if (fieldType.equals(BigInteger.class)) {
+			if (allowLossyConversion)
+				buffer.putInt(BigInteger.class.cast(fieldValue).intValue());
+			else {
+				if (!isSilent)
+					throw new BobcException(BobcErrorCodes.LOSSY_CONVERSION,
+							BobcErrorCodes.lossyConversionMsg(fieldType, Int32Field.class));
+			}
+		} else if (fieldType.equals(String.class)) {
+			buffer.putInt(Integer.valueOf(String.class.cast(fieldValue)));
+		} else {
+			if (!isSilent)
+				throw new BobcException(BobcErrorCodes.UNKNOWN_CONVERSION,
+						BobcErrorCodes.conversionNotKnown(fieldType, Int32Field.class));
+			// we should atleast init the buffer as dummy
+			buffer.putInt(0);
+		}
+		return buffer.array();
 	}
 
 }
--- bobc/src/test/java/bobc/UsageTest.java
+++ bobc/src/test/java/bobc/UsageTest.java
@@ -1,36 +1,36 @@
 package bobc;
 
 import java.lang.reflect.InvocationTargetException;
+import java.math.BigInteger;
 import java.nio.ByteBuffer;
 
 import bobc.core.ByteOrder;
 import bobc.core.struct.ByteOrdering;
 import bobc.core.struct.Struct;
-import bobc.types.ShortField;
-import bobc.types.UShortField;
+import bobc.types.Int32Field;
 
 public class UsageTest {
 
 	@ByteOrdering(ByteOrder.LITTLE_ENDIAN)
 	static public class TT extends Struct<TT> {
-		@ShortField
-		public Short var;
-
-		@ShortField
-		public String test;
-
-		@UShortField(allowLossyConversionFrom = true)
-		public Integer value;
-
-		@UShortField
-		public String sval;
-
-		@ShortField
-		public char c;
-
-		@UShortField(allowLossyConversionFrom = true)
-		public Double d;
-
+		@Int32Field(allowLossyConversionTo = true)
+		public Byte bytef = (byte) 1;
+		@Int32Field(allowLossyConversionTo = true)
+		public char charf = '9';
+		@Int32Field(allowLossyConversionTo = true)
+		public Short shortf = (short) 2;
+		@Int32Field
+		public Integer intf = 3;
+		@Int32Field(allowLossyConversionFrom = true)
+		public Long longf = 4L;
+		@Int32Field(allowLossyConversionFrom = true)
+		public Float floatf = 5.0f;
+		@Int32Field(allowLossyConversionFrom = true)
+		public Double doublef = 6.0;
+		@Int32Field(allowLossyConversionFrom = true)
+		public BigInteger bigintf = BigInteger.valueOf(7);
+		@Int32Field
+		public String stringf = "8";
 	}
 
 	public static void fn(Object... arguments) {
@@ -48,14 +48,20 @@ public class UsageTest {
 			IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 
 		TT test = new TT();
+		test.bytef = (byte) 1;
+		test.charf = '2';
+		test.shortf = (short) 3;
+		test.intf = 4;
+		test.longf = 5L;
+		test.floatf = 6.0f;
+		test.doublef = 7.0;
+		test.bigintf = BigInteger.valueOf(8);
+		test.stringf = "9";
 
-		test.var = (short) 1;
-		test.test = "2";
-		test.value = -1;
-		test.sval = "67";
-		test.c = '1';
+		TT t2 = new TT();
+		t2 = test.newInstanceFrom(test.toBytes());
 
-		TT t = test.newInstanceFrom(test.toBytes());
-		System.out.println(t.var + " " + t.value + " " + t.sval + ">" + t.c + " " + t.d);
+		System.out.println(t2.bytef + " " + t2.charf + " " + t2.shortf + " " + t2.intf + " " + t2.longf + " "
+				+ t2.floatf + " " + t2.doublef + " " + t2.bigintf + " " + t2.stringf);
 	}
 }
